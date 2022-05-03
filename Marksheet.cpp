89ff440a46f088d850067373787f3218362498f1
Vivek Mani Charan P <cs19b032@iittp.ac.in>
2022-05-03 12:18:09 +0530

    ......

--- Marksheet.cpp
+++ Marksheet.cpp
@@ -1,4 +1,28 @@
-#include "Marksheet.h"
+#include <iostream>
+#include <ctype.h>
+#include <sstream>
+#include <cstdlib>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <termios.h>
+#include <time.h>
+#include <unistd.h>
+#include <vector>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+#define TAB_STOP 8
+#define QUIT_TIMES 3
 
 
 enum editorKey {
